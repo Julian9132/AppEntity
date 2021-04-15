d24e4e6ca1109480db6e6fa49353860c01130984
Julian9132 <79948972+Julian9132@users.noreply.github.com>
2021-04-14 21:05:28 -0500

    Add files via upload

---
+++ Estudiante.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Evaluacion
+{
+    public class Estudiante
+    {
+  
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public int Codigo { get; set; }
+        public double  Nota1 { get; set; }
+        public double Nota2 { get; set; }
+        public double Nota3 { get; set; }
+        public double  Promedio { get; set; }
+        public string Concepto { get; set; }
+    }
+
+}
